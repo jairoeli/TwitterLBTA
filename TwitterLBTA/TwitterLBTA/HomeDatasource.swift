17ddc1c1a8bc482552eb72c2d41605fbc479e931
jairoeli <jairoelid@gmail.com>
2017-01-25 10:56:32 -0600

    Updated the information for Tweet model.

--- TwitterLBTA/TwitterLBTA/HomeDatasource.swift
+++ TwitterLBTA/TwitterLBTA/HomeDatasource.swift
@@ -20,7 +20,14 @@ class HomeDatasource: Datasource {
     return [jairoUser, pWUser, kindleCourseUser]
   }()
   
-  let tweets = ["tweet1", "tweet2"]
+  let tweets: [Tweet] = {
+    let jairoUser = User(name: "Jairo Eli de León", username: "@jairoelid", bioText: "iOS engineer, originally from the land of the breakfast taco. @GallaudetU & @DevMtn alum. Spaghetti enthusiast.", profileImage: #imageLiteral(resourceName: "profile_image"))
+    
+    let tweet = Tweet(user: jairoUser, message: "Passion is the only resource you can afford. It helps you to realize your dream because it forces you to keep that dream real. #PowerOfBroke")
+    let tweet2 = Tweet(user: jairoUser, message: "To me, programming is more than an important practical art. It is also a gigantic undertaking in the foundations of knowledge. - Grace Hopper")
+    
+    return [tweet, tweet2]
+  }()
   
   // MARK: - Register classes
   override func footerClasses() -> [DatasourceCell.Type]? {
@@ -38,6 +45,9 @@ class HomeDatasource: Datasource {
   // MARK: - Data source
   
   override func item(_ indexPath: IndexPath) -> Any? {
+    if indexPath.section == 1 {
+      return tweets[indexPath.item]
+    }
     return users[indexPath.item]
   }
   
