0bb19aacd7a012d72519d5938270c8744dd8ee38
jairoeli <jairoelid@gmail.com>
2017-01-25 10:54:05 -0600

    Second model object: Tweet

---
+++ TwitterLBTA/TwitterLBTA/Tweet.swift
@@ -0,0 +1,14 @@
+//
+//  Tweet.swift
+//  TwitterLBTA
+//
+//  Created by Jairo Eli de Leon on 1/25/17.
+//  Copyright © 2017 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet {
+  let user: User
+  let message: String
+}
